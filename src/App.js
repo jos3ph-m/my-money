3c5a1f55c03c24f258bef04d3b51b8d7865f6049
Joseph M <jmedina1985@hotmail.com>
2022-03-07 14:49:34 -0800

    Creating pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
-export default App
+export default App;
