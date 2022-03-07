2041cf311833c495f28bd0fce395328bce816a2e
Joseph M <jmedina1985@hotmail.com>
2022-03-07 14:56:02 -0800

    creating pages and routes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
