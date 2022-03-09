7623481edea1ead5ed8615e657450fb8035a64b4
Joseph M <jmedina1985@hotmail.com>
2022-03-09 12:56:18 -0800

    Making a login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,18 @@
 import styles from './Login.module.css';
 
 export default function Login() {
-  return <form className={styles.['login-form']}><h2>Login</h2><label><span>email:</span><input type="email" /></label></form>;
+  return (
+  <form className={styles.['login-form']}>
+    <h2>Login</h2>
+  <label>
+    <span>email:</span>
+    <input 
+    type="email" 
+    />
+  </label>
+  <label>
+    <span>password</span>
+    <input type="password" />
+  </label>
+  </form>);
 }
