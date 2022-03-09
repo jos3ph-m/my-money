4d078de821f260b8a24c21314469d942ea651035
Joseph M <jmedina1985@hotmail.com>
2022-03-09 12:59:35 -0800

    Making a login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,16 +1,18 @@
+import { useState } from 'react/cjs/react.production.min';
 import styles from './Login.module.css';
 
 export default function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  
+
   return (
   <form className={styles.['login-form']}>
     <h2>Login</h2>
   <label>
     <span>email:</span>
     <input 
-    type="email" 
+    type="email"
+    onChange={(e) => setEmail(e.target.value)} 
     />
   </label>
   <label>
