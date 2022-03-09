1715a1332a03c1261dcb0c69e4a9bd64db280e21
Joseph M <jmedina1985@hotmail.com>
2022-03-08 16:02:37 -0800

    Creating a Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
+import { Link } from 'react-router-dom';
+
+// styles
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}></li>
+        <li className={styles.title}>myMoney</li>
+        <li>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign up</Link>
+        </li>
       </ul>
     </nav>
   );
