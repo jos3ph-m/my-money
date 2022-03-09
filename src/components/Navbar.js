15a3f8e71eb9e311167b046e371e60f9ffcc588c
Joseph M <jmedina1985@hotmail.com>
2022-03-08 16:01:20 -0800

    Creating a Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
-  return <nav className={styles.navbar}></nav>;
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}></li>
+      </ul>
+    </nav>
+  );
 }
