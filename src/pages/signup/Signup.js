952b71f7342a6ef9599bd1720532ed5348b547e3
Joseph M <jmedina1985@hotmail.com>
2022-03-11 14:27:08 -0800

    Making the signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,6 +8,23 @@ export default function Signup() {
   const [password, setPassword] = useState('');
   return (
     <form className={styles.['signup-form']}>
+      <h2>Signup</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
     </form>
   )
 }
