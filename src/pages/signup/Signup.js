9f8cc913263824c74985f07e577e13c27ea7afe8
Joseph M <jmedina1985@hotmail.com>
2022-03-11 14:26:41 -0800

    Making the signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -6,5 +6,8 @@ import styles from './Signup.module.css';
 export default function Signup() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  return <div>Sign up</div>;
+  return (
+    <form className={styles.['signup-form']}>
+    </form>
+  )
 }
