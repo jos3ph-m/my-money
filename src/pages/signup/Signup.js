a8a47a84095412c6f5897396c07601733a864912
Joseph M <jmedina1985@hotmail.com>
2022-03-11 14:26:06 -0800

    Making the signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,10 @@
+import { useState } from 'react/cjs/react.production.min';
+
+// styles
 import styles from './Signup.module.css';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return <div>Sign up</div>;
 }
