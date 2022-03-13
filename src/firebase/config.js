d630341bd37e9177488dc718db79ee2e3ef5a450
Joseph M <jmedina1985@hotmail.com>
2022-03-13 14:40:24 -0700

    firebase setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  authDomain: 'mymoney-1d0a8.firebaseapp.com',
+  projectId: 'mymoney-1d0a8',
+  storageBucket: 'mymoney-1d0a8.appspot.com',
+  messagingSenderId: '866306161880',
+};
