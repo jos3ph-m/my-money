446d6341059c25bff2287a0147b4b4e8df77e752
Joseph M <jmedina1985@hotmail.com>
2022-03-13 14:40:48 -0700

    firebase setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: 'mymoney-1d0a8.firebaseapp.com',
   projectId: 'mymoney-1d0a8',
   storageBucket: 'mymoney-1d0a8.appspot.com',
