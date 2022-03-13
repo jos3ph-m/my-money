ab95b50ab0d4e949f77008bc5b86d151a12435f8
Joseph M <jmedina1985@hotmail.com>
2022-03-12 19:32:04 -0800

    Making the signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -16,30 +16,7 @@ export default function Signup() {
   return (
     <form onSubmit={handleSubmit} className={styles['signup-form']}>
       <h2>Signup</h2>
-      <label>
-        <span>email:</span>
-        <input
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>password:</span>
-        <input
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      <label>
-        <span>display name:</span>
-        <input
-          type="text"
-          onChange={(e) => setDisplayName(e.target.value)}
-          value={displayName}
-        />
-      </label>
+
       <button className="btn">Signup</button>
     </form>
   );
