611f6aeb016e4edd4b48662f07828ee680428e2b
Joseph M <jmedina1985@hotmail.com>
2022-03-12 19:31:16 -0800

    Making the signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -10,7 +10,7 @@ export default function Signup() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password, displayName);
+    console.log(email, password);
   };
 
   return (
