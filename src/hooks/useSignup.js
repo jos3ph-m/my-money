43f81bee5c53398cb27b22d3bb7e0d6a717b6a15
Joseph M <jmedina1985@hotmail.com>
2022-03-14 15:42:31 -0700

    Creating a signup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -4,4 +4,6 @@ import { projectAuth } from '../firebase/config';
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+
+  const signup = async () => {};
 };
