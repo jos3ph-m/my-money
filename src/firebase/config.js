fbb88eb347357d9c72217b47322696e3d8fdc4bf
Joseph M <jmedina1985@hotmail.com>
2022-03-17 16:25:55 -0700

    Using the sign up hook

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+require('dotenv').config();
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: 'mymoney-1d0a8.firebaseapp.com',
   projectId: 'mymoney-1d0a8',
   storageBucket: 'mymoney-1d0a8.appspot.com',
   messagingSenderId: '866306161880',
-  appId: process.env.FIREBASE_APP_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // init firebase
