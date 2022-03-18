35180b5c560ab7d83930cab5dbe3ca06fc7d5a23
Joseph M <jmedina1985@hotmail.com>
2022-03-18 15:12:55 -0700

    Creating auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,6 +2,8 @@ import { createContext, useReducer } from 'react';
 
 export const AuthContext = createContext();
 
+export const authReducer = (state, action) => {};
+
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
