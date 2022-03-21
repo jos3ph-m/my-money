6abb16617a44ec6215e55383dac95f22639d9095
Joseph M <jmedina1985@hotmail.com>
2022-03-20 18:11:24 -0700

    Creating auth context

--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -5,6 +5,7 @@ export const useAuthContext = () => {
   const context = useContext(AuthContext);
 
   if (!context) {
+    throw Error('useAuthContext must be inside an AuthContextProvider');
   }
 
   return context;
