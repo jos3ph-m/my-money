7e6837485b72e3ed3e35bae0a78eef69d409bd08
Joseph M <jmedina1985@hotmail.com>
2022-03-21 16:19:56 -0700

    Creating auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,8 +4,6 @@ export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
   switch (action.type) {
-    case 'LOGIN':
-      return { ...state, user: action.payload };
     default:
       return state;
   }
