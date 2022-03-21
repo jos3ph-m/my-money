31d67cd72b19c05bb21d31747ee1096e800dc077
Joseph M <jmedina1985@hotmail.com>
2022-03-21 16:25:27 -0700

    Dispatching a Login Action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -16,7 +16,6 @@ export const AuthContextProvider = ({ children }) => {
     user: null,
   });
 
-  console.log('AuthContext state: ', state);
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
