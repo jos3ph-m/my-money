f97daaff0fa3f2e871bc4823b4d1f9dacaf09281
Joseph M <jmedina1985@hotmail.com>
2022-03-22 15:04:14 -0700

    Logout hook

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -5,4 +5,5 @@ import { useAuthContext } from './useAuthContext';
 const useLogout = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 };
