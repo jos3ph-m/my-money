52dab505df552350512e1c8f708efbe26ff2c993
Joseph M <jmedina1985@hotmail.com>
2022-03-24 12:37:24 -0700

    Time for a Login Hook

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -2,4 +2,9 @@ import { useState, useEffect } from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
-export const useLogin = () => {};
+export const useLogin = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+};
