ded0d587086cf1f6fc3d241e2433765ccfc1fc64
Joseph M <jmedina1985@hotmail.com>
2022-03-28 13:39:08 -0700

    Firebase auth state changes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,7 +11,7 @@ export const authReducer = (state, action) => {
     case 'LOGOUT':
       return { ...state, user: null };
     case 'AUTH_IS_READY':
-      return {};
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }
