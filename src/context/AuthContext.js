23ffca55fc8d48cfa93e97bf1400f053b3f96441
Joseph M <jmedina1985@hotmail.com>
2022-03-28 13:38:22 -0700

    Firebase auth state changes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -24,6 +24,7 @@ export const AuthContextProvider = ({ children }) => {
   useEffect(() => {
     const unsub = projectAuth.onAuthStateChanged((user) => {
       dispatch({ type: 'AUTH_IS_READY', payload: user });
+      unsub();
     });
   }, []);
 
