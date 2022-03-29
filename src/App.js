d511b71770fb3d5bbc049de3f80f82b2cd50ac14
Joseph M <jmedina1985@hotmail.com>
2022-03-29 13:43:15 -0700

    Route guarding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { useAuthContext } from './hooks/useAuthContext';
 
 // Pages & Components
