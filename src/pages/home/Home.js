425575e4587a562249932f8c786449f17397a73f
Joseph M <jmedina1985@hotmail.com>
2022-03-30 14:34:32 -0700

    Making a transaction form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,7 @@ import styles from './Home.module.css';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div>transaction list</div>
+      <div className={styles.content}>transaction list</div>
       <div></div>
     </div>
   );
