1fe0aa6026cf3d31b3045faa790895286e4e05af
Joseph M <jmedina1985@hotmail.com>
2022-03-31 13:38:56 -0700

    Making a transaction form

--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -3,5 +3,9 @@ import { useState } from 'react';
 export default function TransactionForm() {
   const [name, setName] = useState('');
   const [amount, setAmount] = useState('');
-  return <></>;
+  return (
+    <>
+      <h3>Add a Transaction</h3>
+    </>
+  );
 }
