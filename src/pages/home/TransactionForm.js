130ec0bcd0a5e86ce66128807417c02b1623c6d6
Joseph M <jmedina1985@hotmail.com>
2022-03-31 14:08:52 -0700

    Making a transaction form

--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -6,7 +6,7 @@ export default function TransactionForm() {
   return (
     <>
       <h3>Add a Transaction</h3>
-      <form>
+      <form onSubmit={}>
         <label>
           <span>Transaction name:</span>
           <input
