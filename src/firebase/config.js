d3945da76a87ac39cf203c07cb10f985e839071e
Joseph M <jmedina1985@hotmail.com>
2022-04-05 16:20:16 -0700

    Firestore timestamp

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -23,4 +23,4 @@ const projectAuth = firebase.auth();
 // timestamp
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth };
+export { projectFirestore, projectAuth, timestamp };
