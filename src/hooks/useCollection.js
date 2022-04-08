dd5a851b0ad1a0b2ac7f756197e5d4a6663b0dc6
Joseph M <jmedina1985@hotmail.com>
2022-04-08 13:53:53 -0700

    Creating useCollection hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -3,7 +3,6 @@ import { projectFirestore } from '../firebase/config';
 
 export const useCollection = (collection) => {
   const [documents, setDocuments] = useState(null);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     let ref = projectFirestore.collection(collection);
