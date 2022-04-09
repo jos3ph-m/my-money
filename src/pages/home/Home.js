5342814b5abb3e994f7812f14644d1d994bb8c56
Joseph M <jmedina1985@hotmail.com>
2022-04-09 15:12:35 -0700

    Listing transactions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
   const { documents, error } = useCollection('transactions');
   return (
     <div className={styles.container}>
-      <div className={styles.content}>transaction list</div>
+      <div className={styles.content}></div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
