c9286fc19d7498a67a36b2dea23d424f1928d28d
Joseph M <jmedina1985@hotmail.com>
2022-04-08 17:20:23 -0700

    Creating a useCollection hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -11,7 +11,7 @@ export const useCollection = (collection) => {
     const unsubscribe = ref.onSnapshot((snapshot) => {
       let results = [];
       snapshot.docs.forEach((doc) => {
-        results.push({ ...doc.data() });
+        results.push({ ...doc.data(), id: doc.id });
       });
     });
   }, [collection]);
