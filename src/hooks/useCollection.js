73e26c96e5d02804b4de3f7990249d0fb7432650
Joseph M <jmedina1985@hotmail.com>
2022-04-08 17:21:56 -0700

    Creating a useCollection hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -8,15 +8,20 @@ export const useCollection = (collection) => {
   useEffect(() => {
     let ref = projectFirestore.collection(collection);
 
-    const unsubscribe = ref.onSnapshot((snapshot) => {
-      let results = [];
-      snapshot.docs.forEach((doc) => {
-        results.push({ ...doc.data(), id: doc.id });
-      });
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
 
-      // update state
-      setDocuments(results);
-      setError(null);
-    });
+        // update state
+        setDocuments(results);
+        setError(null);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }, [collection]);
 };
