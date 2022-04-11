be530baf8c24d3eba0e50494b078e2bcd376b1e0
Joseph M <jmedina1985@hotmail.com>
2022-04-11 12:18:27 -0700

    Listing transactions

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -3,6 +3,6 @@ import React from 'react';
 // styles
 import styles from './Home.modlues.css';
 
-export default function TransactionList() {
+export default function TransactionList({ transactions }) {
   return <ul className={styles.transactions}></ul>;
 }
