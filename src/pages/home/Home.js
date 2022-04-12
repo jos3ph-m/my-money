b2cd4ca7363a8bffb0d433965c38f51760f95e6f
Joseph M <jmedina1985@hotmail.com>
2022-04-12 11:56:24 -0700

    Listing transactions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,7 +10,11 @@ import TransactionList from './TransactionList';
 
 export default function Home() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection('transactions');
+  const { documents, error } = useCollection('transactions', [
+    'uid',
+    '==',
+    user.uid,
+  ]);
   return (
     <div className={styles.container}>
       <div className={styles.content}>
