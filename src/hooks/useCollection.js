89fffeb6a676f8872ee8f75b641087462f6e84e5
Joseph M <jmedina1985@hotmail.com>
2022-04-12 12:00:59 -0700

    Firestore queries

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { projectFirestore } from '../firebase/config';
 
 export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
+
+  const query = useRef(_query).current;
 
   useEffect(() => {
     let ref = projectFirestore.collection(collection);
