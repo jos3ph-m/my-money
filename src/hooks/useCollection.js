e4a14102465c3fb444c5cf2183075c79d08c7db2
Joseph M <jmedina1985@hotmail.com>
2022-04-12 12:02:26 -0700

    Firestore queries

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -5,7 +5,7 @@ export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
-  // if we don't use a ref --> infinite loop in useEffect
+  // if we don't use a ref then there will be an infinite loop in useEffect
   // _query is an array and is 'different' on every function call
 
   const query = useRef(_query).current;
