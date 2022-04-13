9ea4b53b95a6cc506fe4fa143c53d74c9af13de7
Joseph M <jmedina1985@hotmail.com>
2022-04-13 16:10:21 -0700

    Ordering Firestore queries

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -19,6 +19,7 @@ export const useCollection = (collection, _query, _orderBy) => {
     }
 
     if (orderBy) {
+      ref = ref.orderBy(...orderBy);
     }
 
     const unsubscribe = ref.onSnapshot(
