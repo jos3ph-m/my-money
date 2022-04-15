438783ce2b524f58521d63c014590a90d49f2bd8
Joseph M <jmedina1985@hotmail.com>
2022-04-15 13:42:15 -0700

    Deleting transactions

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -24,7 +24,6 @@ const firestoreReducer = (state, action) => {
         isPending: false,
         document: action.payload,
         success: true,
-        error: null,
       };
     case 'ERROR':
       return {
