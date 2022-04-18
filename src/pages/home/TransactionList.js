2976783406066d530ae29d543ac2ff576689b38e
Joseph M <jmedina1985@hotmail.com>
2022-04-18 13:15:16 -0700

    Deleting transactions

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -4,6 +4,7 @@ import { useFirestore } from '../../hooks/useFirestore';
 import styles from './Home.module.css';
 
 export default function TransactionList({ transactions }) {
+  const { deleteDocument } = useFirestore('transactions');
   return (
     <ul className={styles.transactions}>
       {transactions.map((transaction) => (
