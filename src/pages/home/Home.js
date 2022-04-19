bd118f15d0b5d8e1166686fab657c8cb215734a8
Joseph M <jmedina1985@hotmail.com>
2022-04-19 13:32:55 -0700

    Firestore rules

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,11 +10,10 @@ import TransactionList from './TransactionList';
 
 export default function Home() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection(
-    'transactions',
-
-    ['createdAt', 'desc']
-  );
+  const { documents, error } = useCollection('transactions', null, [
+    'createdAt',
+    'desc',
+  ]);
   return (
     <div className={styles.container}>
       <div className={styles.content}>
