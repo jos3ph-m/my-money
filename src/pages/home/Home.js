cd2bec7555f2b33951605b5fe34e1eef195a32f6
Joseph M <jmedina1985@hotmail.com>
2022-04-21 15:05:01 -0700

    Deploying firestore rules

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -10,10 +10,11 @@ import TransactionList from './TransactionList';
 
 export default function Home() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection('transactions', null, [
-    'createdAt',
-    'desc',
-  ]);
+  const { documents, error } = useCollection(
+    'transactions',
+    ['uid', '==', user.uid],
+    ['createdAt', 'desc']
+  );
   return (
     <div className={styles.container}>
       <div className={styles.content}>
